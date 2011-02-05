7a748ccfac2df7124da617e39c432e13191c20d4
Alexander Urieles <aeurielesn@gmail.com>
2011-02-05 00:22:10 -0500

    Add state machine json parser and update background methods

--- js/background.js
+++ js/background.js
@@ -4,62 +4,120 @@
 MouseHuntAlerter = {};
 
 /**
+* Error notifications constants. 
+*/
+var NETWORK_ERROR = 0;
+var WRONG_PAGE_ERROR = 1;
+var UNEXPECTED_ERROR = 255;
+
+/**
+* Constants.
+*/
+var DEFAULT_RETRY_MILLISECONDS = 120000;
+
+/**
 * Shows a notification.
 */
 MouseHuntAlerter.showNotification = function(msg) {
-	var notification = webkitNotifications.createNotification(
-		'/icons/text-html-48.png',
-		'Next Active Turn',
-		msg
-	);
-	notification.show();
-}
+    var notification = webkitNotifications.createNotification(
+        '/icons/mouse-and-cheese-48.png',
+        chrome.i18n.getMessage('notification_title'),
+        chrome.i18n.getMessage(msg)
+    );
+//	notification.ondisplay = function() {
+//		setTimeout(function() { notification.cancel(); }, 15000);
+//	}
+    notification.show();
+};
+
+/**
+* Shows an error notification.
+*/
+MouseHuntAlerter.showError = function(status) {
+    var icon = '/icons/dialog-error-48.png';
+    var message = chrome.i18n.getMessage('unexpected_error');
+
+    switch(status)
+    {
+    case NETWORK_ERROR:
+        icon = '/icons/network-error-48.png';
+        message = chrome.i18n.getMessage('network_error');
+        break;
+    case WRONG_PAGE_ERROR:
+        message = chrome.i18n.getMessage('wrong_page_error');
+        break;
+    }
+
+    var notification = webkitNotifications.createNotification(
+        icon,
+        chrome.i18n.getMessage('notification_error_title'),
+        message
+    );
+    notification.ondisplay = function() {
+        setTimeout(function() { notification.cancel(); }, 15000);
+    };
+    notification.show();
+};
 
 /**
 * Fetch the remaining time until the player can sound the Hunter's Horn. 
 */
-MouseHuntAlerter.getNextActiveTurn = function() {
-	var xhr = new XMLHttpRequest();
-    var url = "http://www.mousehuntgame.com/index.php";
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState != 4) return;
-        if(xhr.status == 200) {
-            if(xhr.responseText.match(/appname = 'MouseHunt'/)) {
-            	var nextActiveTurn = xhr.responseText.match(/"next_activeturn_seconds":(\d+)/i);
-            	nextActiveTurn = nextActiveTurn[1] * 1000;
-            	if(nextActiveTurn <= 0) {
-            		MouseHuntAlerter.showNotification("You can sound the Hunter's Horn now!");
-            		var activeTurnWait = xhr.responseText.match(/"activeturn_wait_seconds":(\d+)/i);
-            		activeTurnWait = activeTurnWait[1] * 1000;
-            		setTimeout(MouseHuntAlerter.getNextActiveTurn, activeTurnWait);
-            	} else {
-            		setTimeout(MouseHuntAlerter.getNextActiveTurn, nextActiveTurn);
-            	}
-            } else {
-            	MouseHuntAlerter.showNotification("Something went wrong.");
-                return;
-            }
-        } else if(xhr.status == 404) {
-        	MouseHuntAlerter.showNotification("Server not found.");
-            return;
-        } else {
-        	MouseHuntAlerter.showNotification("Unexpected error while fetching MH.");
-        	return;
-        }
+MouseHuntAlerter.getUserObject = function(response) {
+    var start = response.indexOf("user =");
+    var user = {};
+    if(start !== -1) {
+        start += "user =".length;
+        user = json_parse(response.substr(start));
+    } else {
+        // What to do in case of not find an user object?
     }
+    return user;
+};
+
+/**
+* Check whether the player can sound the Hunter's Horn at the moment.
+* Otherwise schedules a new check.
+*/
+MouseHuntAlerter.checkNextActiveTurn = function() {
+    var xhr = new XMLHttpRequest();
+    var url = "http://apps.facebook.com/mousehunt/index.php";
+    //var url = "http://www.mousehuntgame.com/index.php";
+    xhr.onreadystatechange = function() {
+        var delay = DEFAULT_RETRY_MILLISECONDS;
+        if(xhr.readyState !== 4) {
+            return;
+        } else if(xhr.status === 200) {
+            if(xhr.responseText.match(/appname = 'MouseHunt'/)) {
+                var user = MouseHuntAlerter.getUserObject(xhr.responseText);
+                if(user.next_activeturn_seconds <= 0) {
+                    MouseHuntAlerter.showNotification('sound_hunter_horn');
+                    delay = user.activeturn_wait_seconds * 1000;
+                } else {
+                    delay = user.next_activeturn_seconds * 1000;
+                }
+            } else {
+                MouseHuntAlerter.showError(WRONG_PAGE_ERROR);
+            }
+        } else if(xhr.status === 404) {
+            MouseHuntAlerter.showError(NETWORK_ERROR);
+        } else {
+            MouseHuntAlerter.showError(UNEXPECTED_ERROR);
+        }
+        setTimeout(MouseHuntAlerter.checkNextActiveTurn, delay);
+    };
     try {
         xhr.open("GET", url, true);
         xhr.send(null);
     } catch(e) {
-    	// Unknown error
+        MouseHuntAlerter.showError(UNEXPECTED_ERROR);
     }
-}
+};
 
 /**
 * Called when page is loaded.
 */
 function init() {
-	MouseHuntAlerter.getNextActiveTurn();
+    MouseHuntAlerter.checkNextActiveTurn();
 }
 
 /**
